ee0e2c9a5805f336df71f950527547ee56eba2f8
helaska <helenaschwarz1512@gmail.com>
2024-12-28 20:10:48 +0000

    Mocker

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import random
+import time
 
-st.title("🎈 My new app")
+# Funkcja symulująca odczyt
+def simulate():
+   return random.choice([0, 1])
+def generate():
+ while True:
+    yield simulate()
+    time.sleep(1)
+
+st.title("Operowanie przyciskiem")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "mocker"
 )
+placeholder = st.empty()
+dane = []
+for data in generate():
+    dane.append(data)
+    if len(dane) > 100:
+        dane.pop(0)
+    placeholder.line_chart(dane)
+    time.sleep(1)
